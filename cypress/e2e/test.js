6632c63c0fe5a094557567aec93883bc69a4ff7e
Andy Palacio <45830411+andypalacio@users.noreply.github.com>
2024-05-06 23:44:01 -0300

    implementing login

--- cypress/e2e/test.js
+++ cypress/e2e/test.js
@@ -1,5 +1,17 @@
 const {Given, When, Then, DataTable} = require("@badeball/cypress-cucumber-preprocessor")
+let paymentFields = [];
+let invoiceAmount = 0;
+let invoiceID
 
-Given(/^the user visits google$/, () => {
-    cy.visit('www.google.com')
+after(() => {
+    cy.get('[data-cy="user-menu"]').click();
+    cy.get('[data-cy="logout"]').click();
+})
+Given(/^the user visits ([^"]*)$/, (url) => {
+    cy.visit(url);
+});
+Given(/^the user logs in to the site$/, () => {
+    cy.get('[data-cy="username"]').type('qa@aiwyn.ai');
+    cy.get('[data-cy="password"]').type('password1');
+    cy.get('[data-cy="user-login"]').click();
 });
